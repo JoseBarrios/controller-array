87a7b0fcb63bb5cc1cd39037802cafde795bb07f
JoseBarrios <github@barrios.io>
2024-10-29 19:25:42 -0700

    Documented

--- src/ControllerArray.js
+++ src/ControllerArray.js
@@ -5,6 +5,8 @@ class ControllerArray {
     this.array = array;
   }
 
+  //Time Complexity: O(n), because each element in the array is checked once.
+  //Space Complexity: O(1), as only a constant amount of extra space is needed.
   findMax() {
     let max = -Infinity;
     for (let i = 0; i < this.array.length; i++) {
@@ -16,6 +18,8 @@ class ControllerArray {
     return max;
   }
 
+  //Time Complexity: O(n), because each element in the array is checked once.
+  //Space Complexity: O(1), as only a constant amount of extra space is needed.
   findMin() {
     let min = Infinity;
     for (let i = 0; i < this.array.length; i++) {
@@ -26,6 +30,16 @@ class ControllerArray {
     }
     return min;
   }
+
+    //Time Complexity: 
+    //Space Complexity:
+    findAPeak() {
+    // A peak is defined as an element that is greater than or equal to its neighbors 
+    //1. Choose the middle column of the 2D array.
+	//2. Find the global maximum element in the middle column.
+	//3. Check if the maximum element in this column is a peak
+
+    }
 }
 
 module.exports = ControllerArray;
